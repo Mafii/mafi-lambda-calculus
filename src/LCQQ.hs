89c0050a0cbb422d69027fb5140ebb2c6385152d
Mathias Fischler <mathias.fischler@messerli.ch>
2022-04-19 10:52:33 +0200

    Implement tokenizer

--- src/LCQQ.hs
+++ src/LCQQ.hs
@@ -10,11 +10,12 @@ import qualified Language.Haskell.TH.Lib as TH
 import Language.Haskell.TH.Quote (QuasiQuoter (..))
 import qualified Language.Haskell.TH.Syntax as TH
 import Lib (Term (Abs, App, Var))
+import GHC.IO.Encoding (setLocaleEncoding, utf8)
 
 λ :: QuasiQuoter
 λ =
   QuasiQuoter
-    { quoteExp = parse',
+    { quoteExp = parse,
       quotePat = undefined "quotePat",
       quoteType = undefined "quoteType",
       quoteDec = undefined "quoteDec"
@@ -31,12 +32,71 @@ parse = undefined
 --          | λ<var> . <λexp>
 --          | ( <λexp> <λexp> )
 
-parseVariable :: String -> Q TH.Exp
-parseVariable str = undefined
+data Token = OpeningParenthesis | ClosingParenthesis | LambdaCharacter | LambdaWord | VariableUsageOrBinding String | FunctionAbstractionDot | Space | Newline
+
+instance Show Token where
+  show OpeningParenthesis = "("
+  show ClosingParenthesis = ")"
+  show FunctionAbstractionDot = "<dot>"
+  show LambdaCharacter = "λ"
+  show LambdaWord = "lambda"
+  show Space = "<space>"
+  show Newline = "<newline>"
+  show (VariableUsageOrBinding id) = "<var " ++ id ++ ">"
+
+-- >>> OpeningParenthesis
+-- >>> VariableBinding "abc"
+-- OpeningParenthesis
+-- VariableBinding "abc"
+
+tokenize :: String -> [Token]
+tokenize text = tokenize' text []
+
+tokenize' :: String -> [Token] -> [Token]
+tokenize' "" xs = xs
+-- character λ does not work, encoded variant must be used
+-- if weird errors happen, add setLocaleEncoding utf8 to your code ahead of running this tokenizer
+tokenize' ('\955': restOfString) tokens = tokenize' restOfString (tokens ++ [LambdaCharacter])
+tokenize' ('l':'a':'m':'b':'d':'a': restOfString) tokens = tokenize' restOfString (tokens ++ [LambdaWord])
+tokenize' ('(': restOfString) tokens = tokenize' restOfString (tokens ++ [OpeningParenthesis])
+tokenize' (')': restOfString) tokens = tokenize' restOfString (tokens ++ [ClosingParenthesis])
+tokenize' ('.': restOfString) tokens = tokenize' restOfString (tokens ++ [FunctionAbstractionDot])
+tokenize' (' ': restOfString) tokens = tokenize' restOfString (tokens ++ [Space])
+tokenize' ('\n': restOfString) tokens = tokenize' restOfString (tokens ++ [Newline])
+tokenize' (s:ss) [] = tokenize' ss [VariableUsageOrBinding [s]]
+tokenize' (s:ss) tokens = tokenize' ss (if isVariable (last tokens)
+  then init tokens ++ [appendCharToTokenText (last tokens) s]
+  else tokens ++ [VariableUsageOrBinding [s]])
+
+isVariable :: Token -> Bool
+isVariable (VariableUsageOrBinding _) = True
+isVariable _ = False
+
+appendCharToTokenText :: Token -> Char -> Token
+appendCharToTokenText (VariableUsageOrBinding t) c = VariableUsageOrBinding (t ++ [c])
+appendCharToTokenText _ _ = error "only defined for variables"
+
+lambdaCharacter :: Char
+lambdaCharacter = head "λ"
+
+testCharacterFunc :: Char -> Bool
+testCharacterFunc '\955' = True
+testCharacterFunc _ = False
+
+-- >>> setLocaleEncoding utf8 
+-- >>> tokenize "a"
+-- >>> tokenize "λa. a b"
+-- >>> tokenize "(λb . (λa. a b)) 5"
+-- [<var a>]
+-- [λ,<var a>,<dot>,<space>,<var a>,<space>,<var b>]
+-- [(,λ,<var b>,<space>,<dot>,<space>,(,λ,<var a>,<dot>,<space>,<var a>,<space>,<var b>,),),<space>,<var 5>]
+
+
+
 
 -- leftovers:
 -- working simple non-complete examples
-
+{-
 -- pass in any variable name, get a correct Var Term expression returned
 parse' :: String -> Q TH.Exp
 parse' txt = do
@@ -51,3 +111,4 @@ parse'' txt = do
 
 removeSpaces :: [Char] -> [Char]
 removeSpaces = filter (not . isSpace)
+-}
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,24 +1,25 @@
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TemplateHaskellQuotes #-}
 
 import LCQQ (lambda, λ)
 import Lib (Term (Abs, Var))
-import Test.Hspec
+import Test.Hspec ( hspec, describe, it, shouldBe, Spec )
+import GHC.IO.Encoding (setLocaleEncoding, utf8)
 
 main :: IO ()
 main = hspec spec
 
 spec :: Spec
 spec = do
+  let _ = setLocaleEncoding utf8
   describe "QuasiQuotes for LC" $ do
     it "Creates Var correctly" $
-      ([λ| x |] :: Term) `shouldBe` (Var "x")
+      ([λ| x |] :: Term) `shouldBe` Var "x"
     it "lambda and unicode lambda behave the same" $
       ([λ| x |] :: Term) `shouldBe` ([lambda| x |] :: Term)
     it "Creates lc abstraction correctly" $
-      ([λ| λx. x |] :: Term) `shouldBe` (Abs "x" (Var "x"))
+      ([λ| λx. x |] :: Term) `shouldBe` Abs "x" (Var "x")
 
 -- do
 --describe "QuasiQuotes for LC" $ do
