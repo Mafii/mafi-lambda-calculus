12a5ce1e0aabe75bd1abd4e503ac6d17291e9da8
Mathias Fischler <mafi@fog.im>
2022-04-30 13:30:40 +0200

    Next attempt: Working scopes and application direction

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,6 +1,7 @@
 module Parser (parse) where
 
 import qualified Data.Foldable
+import Data.Maybe (fromJust)
 import Debug.Trace (trace)
 import Lib (Term)
 import qualified Lib
@@ -46,13 +47,15 @@ data AbortReason
   | Parenthesis Int
   deriving (Show, Eq)
 
-type TokenOrStep = Either Token IntermediateParseResult
+data TokenOrScope
+  = T Token
+  | Scope TokenOrScope
+  deriving (Show)
 
 data IntermediateParseResult
   = Var String
-  | Abs String IntermediateParseResult
-  | App CompletedParseResult ParseResult
-  | Scope [TokenOrStep]
+  | Abs String (Maybe IntermediateParseResult)
+  | App IntermediateParseResult IntermediateParseResult
   deriving (Show)
 
 data CompletedParseResult
@@ -61,63 +64,113 @@ data CompletedParseResult
   | AppC CompletedParseResult CompletedParseResult
   deriving (Show)
 
+data Extendable = E IntermediateParseResult [TokenOrScope]
+  deriving (Show)
+
 data ParseResult
   = Completed CompletedParseResult
-  | Partial IntermediateParseResult
-  | Unhandled [TokenOrStep]
+  | Extendable Extendable
+  | Intermediate IntermediateParseResult
+  | Unhandled [TokenOrScope]
   | Error String
   deriving (Show)
 
 parse :: [Tokenizer.Token] -> Term
-parse tokens = toTerm $ ensureCompleted $ parse' $ Unhandled $ generalize $ flatMapTokens tokens
+parse tokens =
+  toTerm $
+    ensureCompleted $
+      parse' $
+        Unhandled $
+          ensureComplete $
+            takeFirstScope $
+              generalize $
+                flatMapTokens tokens
 
-generalize :: [Token] -> [TokenOrStep]
-generalize = map Left
+ensureComplete :: ([TokenOrScope], [TokenOrScope]) -> [TokenOrScope]
+ensureComplete (tokens, []) = tokens
+ensureComplete (_, existing) = error $ "Scope creation error. Unhandled: " ++ show existing
 
--- >>> parse' $ Unhandled $ generalize $ flatMapTokens $ Tokenizer.tokenize "(a(b)c)"
+generalize :: [Token] -> [TokenOrScope]
+generalize = map T
+
+-- >>> parse $ Tokenizer.tokenize "(a(b)c)"
+-- Prelude.undefined
 
 parse' :: ParseResult -> ParseResult
 parse' inp | trace ("parse': " ++ show inp) False = undefined
-parse' u@(Unhandled tokens) = until isCompletedOrError parseStep u
-parse' p@(Partial partial) = until isCompletedOrError parseStep p
+parse' u@(Unhandled tokens) = unpack $ until isCompletedOrError extend (getFirstElement tokens)
+parse' p@(Extendable (E extendable unhandled)) = undefined -- unpack $ until isCompletedOrError extend (appExtender p)
 parse' c@(Completed {}) = c
 parse' e@(Error {}) = e
+parse' val = error $ "could not parse: " ++ show val
 
-parsePartial :: IntermediateParseResult -> ParseResult
-parsePartial input
-  | Scope [Left Lambda, Left Dot, Left (VarUseOrBind id)] <- input = error "missing body for abstraction"
-  | Scope (Left Lambda : Left Dot : Left (VarUseOrBind id) : rest) <- input = Partial $ Abs id (Scope rest)
-  | Scope [Left (VarUseOrBind id)] <- input = Completed $ VarC id
-  | Scope (Left (VarUseOrBind id) : tokens) <- input = Partial $ App (VarC id) (Unhandled tokens)
-  | App completed (Completed c) <- input = Completed $ AppC completed c
-  | App completed (Unhandled []) <- input = Completed completed
-  | App completed unh@(Unhandled elements) <- input = do
-    let (lhs, rest) = case parseStep unh of
-          (Partial (App lhs rest)) -> (lhs, rest)
-          _ -> error "unhandled"
-    let returnValue = Partial $ App (AppC completed lhs) rest
-    trace ("hey: " ++ show returnValue) returnValue
-  | otherwise = undefined
+-- extend
 
-parseStep :: ParseResult -> ParseResult
--- parseStep (Partial Scope) = undefined
-parseStep (Unhandled tokens) = do
-  let (scope, rest) = takeFirstScope tokens
-  Partial $ App (ensureCompleted $ parse' $ Partial $ Scope scope) (Unhandled rest) -- todo here is a bug
-parseStep (Partial (App result (Unhandled []))) = Completed result
-parseStep (Partial partial@(App result (Unhandled tokens))) = parsePartial partial
-parseStep (Partial p@(Scope els)) = parsePartial p
-parseStep val = error $ "parseStep: " ++ show val
+data Extender = Extender
+  { original :: ParseResult,
+    extendf :: IntermediateParseResult -> [TokenOrScope] -> ParseResult
+  }
 
-takeFirstScope :: [TokenOrStep] -> ([TokenOrStep], [TokenOrStep])
+unpack :: Extender -> ParseResult
+unpack = original
+
+extend :: Extender -> Extender
+extend input = do
+  let (r, rest, f) = case input of
+        Extender (Extendable (E interm rest)) f -> (interm, rest, f)
+        Extender (Intermediate interm) f -> (interm, [], \x y -> error "unhandled blub")
+        Extender a b -> error $ "unhandled" ++ show a
+  let next = if null rest then noopExtender r else getFirstElement rest
+  let (newEl, newRest) = case next of
+        Extender (Extendable (E el rest)) _ -> (el, rest)
+        Extender (Intermediate i) _ -> (i, [])
+        _ -> error "unhandled extnext"
+  let result = f newEl newRest
+  let extender = case r of
+        Var id -> appExtender
+        Abs id rhs -> absExtender
+        App lhs rhs -> appExtender
+  if null rest then Extender result (\x y -> error "unhandled blub") else extender result
+
+noopExtender :: IntermediateParseResult -> Extender
+noopExtender i = Extender (Intermediate i) (\x y -> error "can not extend further")
+
+getFirstElement :: [TokenOrScope] -> Extender
+getFirstElement x | trace ("getFirstElement: " ++ show x) False = undefined
+getFirstElement ((Scope inner) : tokens) = appExtender (parse' $ Unhandled [inner])
+getFirstElement (T Lambda : (T (VarUseOrBind id)) : tokens) = absExtender (Extendable (E (Abs id Nothing) tokens))
+getFirstElement (T (VarUseOrBind id) : tokens) = appExtender (Extendable (E (Var id) tokens))
+getFirstElement val = error $ "Unhandled (possibly invalid) sequence: " ++ show val
+
+appExtender :: ParseResult -> Extender
+appExtender (Extendable (E orig ts)) = do
+  let f rhs tks = Extendable (E (App orig rhs) tks)
+  Extender (Extendable (E orig ts)) f
+appExtender _ = error "unexpected app extend"
+
+absExtender :: ParseResult -> Extender
+absExtender (Extendable (E orig@(Abs id body) tks)) = do
+  let extendBody prev append = case prev of
+        Nothing -> append
+        Just body -> App body append
+  let f rhs tks = Extendable (E (Abs id (Just $ extendBody body rhs)) tks)
+  Extender (Extendable (E orig tks)) f
+absExtender _ = error "unexpected abs extend"
+
+-- >>> unpack $ getFirstElement $ generalize [Lambda, VarUseOrBind "a", Dot, VarUseOrBind "a"]
+-- Intermediate (Abs "a" Nothing)
+
+-- scope
+
+takeFirstScope :: [TokenOrScope] -> ([TokenOrScope], [TokenOrScope])
 takeFirstScope tokens = ensureResult $ takeFirstScope' $ Empty tokens
 
 takeFirstScope' :: ScopeAggregator -> ScopeAggregator
 takeFirstScope' aggr
   | r@(Result {}) <- aggr = r
-  | Empty (Left OpenParens : tokens) <- aggr = takeFirstScope' $ Aggregating [] tokens 1
+  | Empty (T OpenParens : tokens) <- aggr = takeFirstScope' $ Aggregating [] tokens 1
   | Empty tokens <- aggr = takeFirstScope' $ Aggregating [] tokens 0
-  | Aggregating els (Left ClosingParens : tokens) depth <- aggr =
+  | Aggregating els (T ClosingParens : tokens) depth <- aggr =
     if depth > 1
       then takeFirstScope' $ Aggregating els tokens (depth - 1)
       else
@@ -125,9 +178,9 @@ takeFirstScope' aggr
           then Result els tokens
           else error "parenthesis mismatch"
   | Aggregating els [next] depth <- aggr = if depth == 0 then Result (els ++ [next]) [] else error "parenthesis mismatch"
-  | Aggregating els (Left OpenParens : tokens) depth <- aggr = do
-    let (scope, rest) = takeFirstScope (Left OpenParens : tokens)
-    takeFirstScope' $ Aggregating (els ++ [Right $ Scope scope]) rest depth
+  | Aggregating els (T OpenParens : tokens) depth <- aggr = do
+    let (scope, rest) = takeFirstScope (T OpenParens : tokens)
+    takeFirstScope' $ Aggregating (els ++ scope) rest depth
   | Aggregating els (next : tokens) depth <- aggr = takeFirstScope' $ Aggregating (els ++ [next]) tokens depth
   | otherwise = error $ "unhandled scope take: " ++ show aggr
 
@@ -142,12 +195,12 @@ takeFirstScope' aggr
 type BracketDepth = Int
 
 data ScopeAggregator
-  = Empty [TokenOrStep]
-  | Aggregating [TokenOrStep] [TokenOrStep] BracketDepth
-  | Result [TokenOrStep] [TokenOrStep]
+  = Empty [TokenOrScope]
+  | Aggregating [TokenOrScope] [TokenOrScope] BracketDepth
+  | Result [TokenOrScope] [TokenOrScope]
   deriving (Show)
 
-ensureResult :: ScopeAggregator -> ([TokenOrStep], [TokenOrStep])
+ensureResult :: ScopeAggregator -> ([TokenOrScope], [TokenOrScope])
 ensureResult (Result scope rest) = (scope, rest)
 ensureResult val = error $ "bug, not finished:: " ++ show val
 
@@ -158,12 +211,22 @@ toTerm (AppC l r) = Lib.App (toTerm l) (toTerm r)
 toTerm (AbsC id r) = Lib.Abs id (toTerm r)
 toTerm (VarC id) = Lib.Var id
 
+toCompleted :: IntermediateParseResult -> CompletedParseResult
+toCompleted (App l r) = AppC (toCompleted l) (toCompleted r)
+toCompleted (Abs id r) = AbsC id (toCompleted $ fromJust r)
+toCompleted (Var id) = VarC id
+
 ensureCompleted :: ParseResult -> CompletedParseResult
 ensureCompleted (Completed c) = c
+ensureCompleted (Extendable (E el [])) = toCompleted el
+ensureCompleted (Intermediate i) = toCompleted i
 ensureCompleted val = error $ "incomplete parse result: " ++ show val
 
-isCompletedOrError :: ParseResult -> Bool
--- isCompletedOrError v | trace ("check completed: " ++ show v) False = undefined
-isCompletedOrError (Completed c) = True
-isCompletedOrError (Error c) = True
-isCompletedOrError _ = False
+isCompletedOrError :: Extender -> Bool
+isCompletedOrError (Extender a b) | trace ("check completed: " ++ show a) False = undefined
+isCompletedOrError ex
+  | (Completed c) <- original ex = True
+  | (Extendable (E e [])) <- original ex = True
+  | (Error c) <- original ex = True
+  | (Intermediate r) <- original ex = False
+  | otherwise = False
