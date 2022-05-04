46380562b599d13c8eebaf1fd3a20ad607c543ed
Mathias Fischler <mafi@fog.im>
2022-05-04 10:08:17 +0200

    Improve alpha eq with name replacement

--- src/Reducer.hs
+++ src/Reducer.hs
@@ -35,12 +35,22 @@ isFreeIn id term = elem id $ getFreeVariables term
 -- True
 -- False
 
+getBoundVariables :: Term -> [Id]
+getBoundVariables (Abs id body) = id : getBoundVariables body
+getBoundVariables (Var id) = []
+getBoundVariables (App lhs rhs) = getBoundVariables lhs ++ getBoundVariables rhs
+
+-- >>> getBoundVariables [lambda| lambda a . y lambda x . 5 7 lambda 55 . a 5 |]
+-- ["a","x","55"]
+
+-- isBoundIn :: Id -> Term -> Bool
+-- isBoundIn id (Abs id' body)
+--   | id == id' = True
+--   | otherwise = id `isBoundIn` body
+-- isBoundIn id (Var id') = False
+-- isBoundIn id (App lhs rhs) = id `isBoundIn` lhs || id `isBoundIn` rhs
 isBoundIn :: Id -> Term -> Bool
-isBoundIn id (Abs id' body)
-  | id == id' = True
-  | otherwise = id `isBoundIn` body
-isBoundIn id (Var id') = False
-isBoundIn id (App lhs rhs) = id `isBoundIn` lhs || id `isBoundIn` rhs
+isBoundIn id term = elem id $ getBoundVariables term
 
 -- >>> "b" `isBoundIn` [lambda| lambda a . lambda b . a c b |]
 -- >>> "c" `isBoundIn` [lambda| lambda a . lambda b . a c b |]
@@ -49,7 +59,8 @@ isBoundIn id (App lhs rhs) = id `isBoundIn` lhs || id `isBoundIn` rhs
 
 alphaConvert :: Term -> Id -> Term
 alphaConvert (Abs oldId body) newId
-  | newId `isFreeIn` body || newId `isBoundIn` body = error "new id must not be bound or free inside abstraction"
+  | newId `isFreeIn` body || newId `isBoundIn` body =
+    error $ "new id must not be bound or free inside abstraction: " ++ show oldId ++ show newId ++ show body
   | otherwise = Abs newId $ conv body oldId newId
   where
     conv (Abs id body) old new = Abs id (conv body old new)
@@ -62,22 +73,43 @@ alphaConvert _ _ = error "can only alpha convert abstractions"
 -- (λa. (λb. ((a c) b)))
 -- (λx. (λb. ((x c) b)))
 
+-- not complete: if outer scope has a variable already, it can not be used in inner scope again
 alphaEq :: Term -> Term -> Bool
 alphaEq (Var id) (Var id') = id == id'
 alphaEq (App lhs rhs) (App lhs' rhs') = lhs `alphaEq` lhs' && rhs `alphaEq` rhs'
 alphaEq lhs@(Abs id body) rhs@(Abs id' body')
   | id == id' = body `alphaEq` body'
-  | not (id' `isFreeIn` body) = alphaConvert lhs id' `alphaEq` rhs
-  | not (id `isFreeIn` body') = alphaConvert rhs id `alphaEq` lhs
-  | otherwise = error "need to pick random identifier to alpha convert lhs and rhs first, not yet done"
+  | not (id' `isFreeIn` body || id' `isBoundIn` body) = alphaConvert lhs id' `alphaEq` rhs
+  | not (id `isFreeIn` body' || id `isBoundIn` body') = alphaConvert rhs id `alphaEq` lhs
+  | otherwise = do
+    let newId = getNonConflictingIdentifier lhs rhs id
+    alphaConvert lhs newId `alphaEq` alphaConvert rhs newId
 alphaEq _ _ = False
+
+getNonConflictingIdentifier :: Term -> Term -> Id -> Id
+getNonConflictingIdentifier term term' identifierBase = do
+  let freeAndBoundVariables =
+        getFreeVariables term ++ getBoundVariables term ++ getFreeVariables term' ++ getBoundVariables term'
+  let number = until (not . flip elem freeAndBoundVariables . (identifierBase ++) . show) (+ 1) 1
+  identifierBase ++ show number
+
+-- >>> getNonConflictingIdentifier [λ| λ a . x |] [λ| λ x . a |] "a"
+-- >>> getNonConflictingIdentifier [λ| λ a . x a4 a7 |] [λ| λ x . a a1 a2 a3 a5 |] "a"
+-- "a1"
+-- "a6"
 
 -- >>> alphaEq [λ| λ a . λ b . a c b |] [λ| λ x . λ b . x c b |]
 -- >>> alphaEq [λ| λ a . x |] [λ| λ x . x |]
 -- >>> alphaEq [λ| λ a . x |] [λ| λ x . a |]
+-- >>> alphaEq [λ| λ x . y |] [λ| λ y . y |]
+-- >>> getNonConflictingIdentifier [λ| λy.λx.y |] [λ| λx.λy.x |] "y"
+-- >>> alphaEq [λ| λy.λx.y |] [λ| λx.λy.x |]
 -- True
 -- False
--- need to pick random identifier to alpha convert lhs and rhs first, not yet done
+-- False
+-- False
+-- "y1"
+-- True
 
 betaConvert :: Term -> Term
 betaConvert (App (Abs id body) rhs)
@@ -98,10 +130,12 @@ replaceIdWithConcreteValue (App lhs rhs) id replacement =
 replaceIdWithConcreteValue (Abs id' body) id replacement = Abs id' (replaceIdWithConcreteValue body id replacement)
 
 -- >>> betaConvert [λ| (λ a . a) b |]
+-- >>> betaConvert [λ| (λ a . a a a) b |]
 -- >>> betaConvert [λ| (λ a . a) (λ a . a) |]
 -- >>> betaConvert [λ| (λ a . λ b . a) (99 * c) |]
 -- >>> betaConvert [λ| (λ a . λ b . a) b |]
 -- b
+-- ((b b) b)
 -- (λa. a)
 -- (λb. ((99 *) c))
 -- Precondition failure: Rhs of reduction contains bound variable conflicting bind in lhs
