50f43caa49513bdcdeafbdf9d7e670023a5f5402
Mathias Fischler <mafi@fog.im>
2022-05-11 19:17:14 +0200

    Use <$> over manual lift

--- src/NewParser.hs
+++ src/NewParser.hs
@@ -1,6 +1,6 @@
 module NewParser (parse, Token (VarUseOrBind, OpenParens, ClosingParens, Dot, Lambda)) where
 
-import Control.Applicative (Alternative (many, (<|>)), Applicative (liftA2), liftA, liftA3)
+import Control.Applicative (Alternative (many, some, (<|>)), Applicative (liftA2), liftA, liftA3)
 import Data.Either (fromLeft)
 import qualified Data.Foldable
 import Data.Functor (($>), (<&>))
@@ -27,23 +27,12 @@ import qualified Tokenizer (Token (..), tokenize)
 parse :: String -> R Term
 parse = completedOrError . runParser parser . flatMapTokens . tokenize
 
--- >>> parse "a b c (d e) f"
--- >>> parse "((((a))b))c"
--- >>> parse "(lambda a . (lambda b . b 7 8) 5) 99"
--- ((((a b) c) (d e)) f)
--- ((a b) c)
--- ((λa. ((λb. ((b 7) 8)) 5)) 99)
-
 parser :: Parser Term
-parser = foldTs $ many (parseScope <|> parseAbs <|> parseVar)
-
-foldTs :: Parser [Term] -> Parser Term
-foldTs p = createParser $ \s -> do
-  (terms, tokens) <- runParser p s
-  case terms of
-    [] -> Error "No terms found, likely empty input"
-    [single] -> Ok (single, tokens)
-    first : terms -> Ok (foldl App first terms, tokens)
+parser = foldt <$> some (parseScope <|> parseAbs <|> parseVar)
+  where
+    foldt :: [Term] -> Term
+    foldt [] = error "unreachable, should only be called in combination with some"
+    foldt (first : terms) = foldl App first terms
 
 parseScope :: Parser Term
 parseScope = getOpeningParenthesis *> parser <* getClosingParenthesis
@@ -51,16 +40,10 @@ parseScope = getOpeningParenthesis *> parser <* getClosingParenthesis
 parseVar :: Parser Term
 parseVar = Var <$> getVar
 
--- >>> runParser parser [VarUseOrBind "A", VarUseOrBind "B", VarUseOrBind "C", VarUseOrBind "D"]
--- ((((A B) C) D),[])
-
-absL :: (Applicative f) => f Id -> f Term -> f Term
-absL = liftA2 Abs
-
 parseAbs :: Parser Term
-parseAbs = absL getLambdaVar parser
+parseAbs = liftA2 Abs getLambdaVar parser
 
--- infrastructure
+-- external to internal token handling
 
 flatMapTokens :: [Tokenizer.Token] -> [Token]
 flatMapTokens = flatMap mapToken
