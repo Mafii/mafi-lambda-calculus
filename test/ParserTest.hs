abe673ef82e087b567fc31394a19c893aa7d82a4
Mathias Fischler <mathias.fischler@messerli.ch>
2023-04-07 16:41:12 +0200

    Remove set-local in every test

--- test/ParserTest.hs
+++ test/ParserTest.hs
@@ -2,7 +2,6 @@
 
 module ParserTest where
 
-import GHC.IO.Encoding (setLocaleEncoding, utf8)
 import LCQQ (lambda, λ)
 import Lib (Term (Abs, App, Var))
 import Parser (parse)
@@ -10,7 +9,6 @@ import Test.Hspec (Spec, describe, it, shouldBe)
 
 spec :: Spec
 spec = do
-  let _ = setLocaleEncoding utf8
   describe "QuasiQuotes for LC" $ do
     it "unicode lambda is equal to lambda" $
       [λ| asdf |] == [lambda| asdf |]
