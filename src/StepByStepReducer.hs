e2c7ed40cbc8c700359ca6c623845ddced11ca73
Mathias Fischler <mathias.fischler@messerli.ch>
2023-04-08 13:43:21 +0200

    Implement innermost reduction, test both strategys together and equally

--- src/StepByStepReducer.hs
+++ src/StepByStepReducer.hs
@@ -1,4 +1,4 @@
-module StepByStepReducer (reduce, Strategy (..), Steps) where
+module StepByStepReducer (reduce, Strategy (..), MaxSteps) where
 
 import Lib (Term (Abs, App, Var))
 import Reducer (betaReduce)
@@ -6,17 +6,38 @@ import Reducer (betaReduce)
 data Strategy = LeftmostOutermost | LeftmostInnermost
   deriving (Show)
 
-type Steps = Int
+type MaxSteps = Int
 
-reduce :: Term -> Strategy -> Steps -> Term
+reduce :: Term -> Strategy -> MaxSteps -> Term
 reduce term _ 0 = term
-reduce _ LeftmostInnermost _ = undefined "not done"
-reduce term LeftmostOutermost steps = reduce (reduce1 term) LeftmostOutermost (steps - 1)
+reduce term strategy steps = reduce (strategyF term) strategy (steps - 1)
+  where
+    strategyF = case strategy of
+      LeftmostOutermost -> reduceOutermost1
+      LeftmostInnermost -> reduceInnermost1
 
--- Does leftmost outermost
+-- TOOD: I am not sure all of this is correct and/or complete :)
+
+-- Some things to read and think about:
+-- "If we restrict the normal order and applicative order strategies so as not to perform reductions inside the body of λ-abstractions, we obtain strategies known as call-by-name (CBN) and call-by-value (CBV), respectively."
+-- Source: https://www.cs.cornell.edu/courses/cs6110/2018sp/lectures/lec04.pdf
+-- CBN: Leftmost outermost strategy
+-- CBV: Leftmost innermost strategy
+
+-- Does leftmost outermost, one time
+-- Description: A function's arguments are substituted into the body of a function before they are reduced. A function's arguments are reduced as often as they are needed.
 -- Remark: In case a normal form exists, the leftmost outermost reduction strategy will find it.
-reduce1 :: Term -> Term
-reduce1 var@Var {} = var
-reduce1 (Abs id term) = Abs id (reduce1 term)
-reduce1 app@(App (Abs {}) _) = betaReduce app
-reduce1 (App lhs rhs) = App (reduce1 lhs) rhs
+reduceOutermost1 :: Term -> Term
+reduceOutermost1 app@(App Abs {} _) = betaReduce app
+reduceOutermost1 (App lhs@App {} rhs) = App (reduceOutermost1 lhs) rhs
+reduceOutermost1 (App lhs rhs) = App lhs (reduceOutermost1 rhs)
+reduceOutermost1 val = val
+
+-- Does leftmost innermost, one time
+-- Description: A function's arguments are substituted into the body of a functionj after they are reduced. A function's arguments are reduced exactly once.
+-- Remark: In case a normal form exists, the leftmost innermost reduction strategy might not find it. It may never terminate.
+reduceInnermost1 :: Term -> Term
+reduceInnermost1 var@Var {} = var
+reduceInnermost1 (App abs@Abs {} app@App {}) = App abs $ reduceInnermost1 app
+reduceInnermost1 app@(App Abs {} _) = betaReduce app
+reduceInnermost1 val = val
--- test/StepByStepReducerTest.hs
+++ test/StepByStepReducerTest.hs
@@ -5,20 +5,30 @@ module StepByStepReducerTest where
 import LCQQ (λ)
 import Lib (Term (..))
 import qualified Reducer (alphaEq)
-import StepByStepReducer (Strategy (..), reduce)
+import StepByStepReducer (MaxSteps, Strategy (..), reduce)
 import Test.Hspec (Spec, describe, it, shouldBe)
+
+-- No test should reach that complexity anyways
+maxReductionAttempts = 20
 
 spec :: Spec
 spec = do
-  describe "Step by Step reduction" $ do
-    it "Reduction with 0 steps is identity operation with LeftmostInnermost" $ do
-      reduce [λ| (λx.x) whatever |] LeftmostInnermost 0 `shouldBe` [λ| (λx.x) whatever |]
-    it "Reduction with 0 steps is identity operation with LeftmostOutermost" $ do
-      reduce [λ| (λx.x) whatever |] LeftmostOutermost 0 `shouldBe` [λ| (λx.x) whatever |]
+  spec' LeftmostOutermost maxReductionAttempts
+  spec' LeftmostInnermost maxReductionAttempts
+
+spec' :: Strategy -> MaxSteps -> Spec
+spec' strategy maxSteps = do
+  describe ("Step by Step reduction with strategy " ++ show strategy) $ do
+    it "Reduction with 0 steps is identity operation" $ do
+      reduce [λ| (λx.x) whatever |] strategy 0 `shouldBe` [λ| (λx.x) whatever |]
     it "Identity lambda is reduced to value when value is applied within one step" $ do
-      reduce [λ| (λx.x) whatever |] LeftmostOutermost 1 `shouldBe` [λ| whatever |]
+      reduce [λ| (λx.x) whatever |] strategy 1 `shouldBe` [λ| whatever |]
+    it "Identity lambda reduction still works with higher max steps allowed" $ do
+      reduce' [λ| (λx.x) whatever |] `shouldBe` [λ| whatever |]
     it "Endless recursion stops correctly at max steps" $ do
-      reduce [λ| (λx.x x) (λx.x x) |] LeftmostOutermost 50 `shouldBe` [λ| (λx.x x) (λx.x x) |]
+      reduce' [λ| (λx.x x) (λx.x x) |] `shouldBe` [λ| (λx.x x) (λx.x x) |]
+    it "Other endless recursion example terminates correctly" $ do
+      reduce' [λ| (λx.λy.y) |] `shouldBe` [λ| (λx.λy.y) |]
   describe "General reduction tests" $ do
     it "Identity lambda is reduced to value when value is applied" $ do
       reduce' [λ| (λx.x) whatever |] `shouldBe` [λ| whatever |]
@@ -30,10 +40,5 @@ spec = do
       reduce' [λ| (λx.λy.x y) y |] `shouldBe` [λ| λy1.y y1 |]
     it "`(λx.λy.x y) y` reduces to a term that is alpha equivalent to `λz.y z`" $ do
       Reducer.alphaEq (reduce' (App (Abs "x" (Abs "y" (App (Var "x") (Var "y")))) (Var "y"))) (Abs "z" (App (Var "y") (Var "z")))
-
--- No test should reach that complexity anyways
-maxReductionAttempts = 20
-
--- makes general tests easier for now - once leftmost innermost is implemented, this has to be changed
-reduce' :: Term -> Term
-reduce' term = reduce term LeftmostOutermost maxReductionAttempts
+  where
+    reduce' lc = reduce lc strategy maxSteps
